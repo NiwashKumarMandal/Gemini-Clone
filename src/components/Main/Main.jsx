8b365311fc535721b35661418237275580e6b0f7
Niwash Kumar Mandal <niwashkumarmandal852@gmail.com>
2024-07-14 15:40:16 +0530

    Add reload function

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -23,7 +23,7 @@ const Main = () => {
   return (
     <div className="main">
       <div className="nav">
-        <p>Gemini</p>
+        <p style={{cursor:"pointer"}} onClick={()=>{location.reload()}}>Gemini</p>
         <img src={assets.user_icon} />
       </div>
       <div className="main-container">
